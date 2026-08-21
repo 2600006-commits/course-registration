737d7709585dbb3dbfef9b48b597df19a59323b7
2600006-commits <2600006@hanseo.sen.hs.kr>
2026-08-21 15:13:02 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,23 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "본인의_API_KEY",
-  authDomain: "본인의_PROJECT_ID.firebaseapp.com",
-  projectId: "본인의_PROJECT_ID",
-  storageBucket: "본인의_PROJECT_ID.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyB60iMwr7KXJvXJfmMIIpzxBmLIfEuLsSw",
+  authDomain: "course-registration-a254b.firebaseapp.com",
+  databaseURL: "https://course-registration-a254b-default-rtdb.firebaseio.com",
+  projectId: "course-registration-a254b",
+  storageBucket: "course-registration-a254b.firebasestorage.app",
+  messagingSenderId: "355584544525",
+  appId: "1:355584544525:web:1f772d5a02d4a569deea44",
+  measurementId: "G-9H8XBHTN70"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
